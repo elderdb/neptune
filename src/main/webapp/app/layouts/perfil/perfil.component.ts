60048fa5327039fc0be539d3538786bb73c8b3fb
elderdb <elderdbspfc@gmail.com>
2019-07-05 19:46:30 -0300

    - Perfil component

---
+++ src/main/webapp/app/layouts/perfil/perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.scss']
+})
+export class PerfilComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
