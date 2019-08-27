9cc38b3597dcb057548123134b302909159fde49
elderdb <elderdbspfc@gmail.com>
2019-08-27 16:34:26 -0300

    - Perfil component

---
+++ src/main/webapp/app/layouts/knowledge/knowledge.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-knowledge',
+  templateUrl: './knowledge.component.html',
+  styleUrls: ['./knowledge.component.scss']
+})
+export class KnowledgeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
