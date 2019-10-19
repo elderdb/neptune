f25f5a0c77bad08f2d9a05334ff1f8cb15a4759c
Élder Dias Batista <elderdbspfc@gmail.com>
2019-10-18 21:52:27 -0300

    - Knowledge icons.

--- src/main/webapp/app/layouts/knowledge/knowledge.component.ts
+++ src/main/webapp/app/layouts/knowledge/knowledge.component.ts
@@ -11,16 +11,94 @@ export class KnowledgeComponent implements OnInit {
   constructor() {
     this.imageObject = [
       {
-        image: '../content/images/java.jpg',
-        thumbImage: '../content/images/java.jpg',
+        image: '../content/images/java.png',
+        thumbImage: '../content/images/java.png',
         alt: 'Java 8',
-        title: null
+        title: 'Java 8'
       },
       {
-        image: '../content/images/rsz_spring.jpg',
-        thumbImage: '../content/images/rsz_spring.jpg',
+        image: '../content/images/spring.png',
+        thumbImage: '../content/images/spring.png',
         alt: 'Spring',
-        title: null
+        title: 'Spring'
+      },
+      {
+        image: '../content/images/angular.png',
+        thumbImage: '../content/images/angular.png',
+        alt: 'AngularJS',
+        title: 'AngularJS'
+      },
+      {
+        image: '../content/images/postgres.png',
+        thumbImage: '../content/images/postgres.png',
+        alt: 'PostgreSQL',
+        title: 'PostgreSQL'
+      },
+      {
+        image: '../content/images/oracle.png',
+        thumbImage: '../content/images/oracle.png',
+        alt: 'Oracle',
+        title: 'Oracle'
+      },
+      {
+        image: '../content/images/aws.png',
+        thumbImage: '../content/images/aws.png',
+        alt: 'AWS',
+        title: 'AWS'
+      },
+      {
+        image: '../content/images/git.png',
+        thumbImage: '../content/images/git.png',
+        alt: 'Git',
+        title: 'Git'
+      },
+      {
+        image: '../content/images/javascript.png',
+        thumbImage: '../content/images/javascript.png',
+        alt: 'JavaScript',
+        title: 'JavaScript'
+      },
+      {
+        image: '../content/images/npm.png',
+        thumbImage: '../content/images/npm.png',
+        alt: 'NPM',
+        title: 'NPM'
+      },
+      {
+        image: '../content/images/bootstrap.png',
+        thumbImage: '../content/images/bootstrap.png',
+        alt: 'Bootstrap',
+        title: 'Bootstrap'
+      },
+      {
+        image: '../content/images/mysql.png',
+        thumbImage: '../content/images/mysql.png',
+        alt: 'MySql',
+        title: 'MySql'
+      },
+      {
+        image: '../content/images/css.png',
+        thumbImage: '../content/images/css.png',
+        alt: 'Css3',
+        title: 'Css3'
+      },
+      {
+        image: '../content/images/xml.png',
+        thumbImage: '../content/images/xml.png',
+        alt: 'XML',
+        title: 'XML'
+      },
+      {
+        image: '../content/images/json.png',
+        thumbImage: '../content/images/json.png',
+        alt: 'Json',
+        title: 'Json'
+      },
+      {
+        image: '../content/images/html.png',
+        thumbImage: '../content/images/html.png',
+        alt: 'HTML5',
+        title: 'HTML5'
       }
     ];
   }
